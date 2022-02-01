65456f224de1f0578bf6157139859fa7a1095a6c
Euller Peixoto <peixotoeuller500@gmail.com>
2022-02-01 17:54:11 -0300

    added material UI, customized the main interface

---
+++ components/CharacterCard/Index.tsx
@@ -0,0 +1,41 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+
+type character = {
+  image: string;
+  name: string;
+  description: string;
+};
+
+export default function CharacterCard({ image, name, description }: character) {
+  return (
+    <Card
+      sx={{
+        m: 1,
+        borderRadius: "0.2em",
+      }}
+      elevation={2}
+    >
+      <CardHeader
+        sx={{
+          backgroundColor: "primary.dark",
+          color: "primary.main",
+          cursor: "default",
+        }}
+        title={name}
+      />
+      <CardMedia component="img" height="194" image={image} alt={name} />
+      <CardContent>
+        <Link href="/" passHref>
+          <Typography component="a" variant="body2" color="text.link">
+            More info about {name} →
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../utils/Theme";
+import type { AppProps } from "next/app";
+
+import Header from "../components/Header/Index";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
