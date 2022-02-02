b920b4b32745e53d093817daf0cc76a3a9da18bc
Euller Peixoto <peixotoeuller500@gmail.com>
2022-02-02 20:06:24 -0300

    page for user search results

---
+++ pages/search-results/[results].tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { GetServerSideProps } from "next";
+
+import getCharacterData from "../../utils/getCharacterData";
+import CharacterCard from "../../components/CharacterCard/Index";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { character } from "../../models/characterType";
+
+const Results = ({ data, title }: any) => {
+  const [results] = useState(data);
+
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12} sx={{ m: 3, display: "grid", placeItems: "center" }}>
+          <Typography
+            component="h4"
+            variant="h6"
+            color="text.link"
+            fontWeight={800}
+          >
+            Here are some results for: {title}...
+          </Typography>
+        </Grid>
+        {results &&
+          results.map(({ id, name, thumbnail }: character) => {
+            return (
+              <Grid item key={id} xs={12} sm={6} md={4} lg={3}>
+                <CharacterCard
+                  image={
+                    `${thumbnail.path}/standard_fantastic.${thumbnail.extension}` ||
+                    ""
+                  }
+                  name={name}
+                  id={id}
+                />
+              </Grid>
+            );
+          })}
+      </Grid>
+    </>
+  );
+};
+
+export default Results;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const data = await getCharacterData(
+    `characters?nameStartsWith=${params?.results}`,
+    50
+  );
+  const newData = data.data.results;
+
+  return {
+    props: {
+      data: newData || [],
+      title: params?.results,
+    },
+  };
+};
