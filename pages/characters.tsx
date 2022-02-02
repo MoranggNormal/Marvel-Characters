8a8e3690c8a3cd4a2409f4f2ed07170c85d65d95
Euller Peixoto <peixotoeuller500@gmail.com>
2022-02-02 04:36:56 -0300

    introduced data and infinite scroll on characters page

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
-import CharacterCard from "../components/CharacterCard/Index";
+import { useState, useEffect, useRef } from "react";
 
+import getMarvelData from "../utils/getMarvelData";
+
+import useOnScreen from "../hooks/useOnScreen";
+
+import CharacterCard from "../components/CharacterCard/Index";
 import Grid from "@mui/material/Grid";
-const mockData = require("../mock/marvelMockData.json");
+import Button from "@mui/material/Button";
 
 type character = {
   id: number;
@@ -14,32 +19,88 @@ type character = {
   };
 };
 
-const Characters: NextPage = () => {
+const Characters: NextPage = ({ data }: any) => {
+  const [results, setResults] = useState(data);
+  const [resultss, setResultss] = useState(data);
+  const [currentData, setCurrentData] = useState(25);
+
+  const isAtBottom = useRef<HTMLDivElement>(null);
+
+  const onScreen = useOnScreen(isAtBottom, "300px");
+
+  useEffect(() => {
+    if (onScreen) {
+      const getNewData = async () => {
+        const data = await getMarvelData("/characters", 25, currentData);
+        const newData = data.data.results;
+
+        setResults((prevState: []) => [...prevState.concat(newData)]);
+        setCurrentData((prevState) => prevState + 25);
+      };
+
+      getNewData();
+    }
+  }, [onScreen]);
+
   return (
     <div>
       <Grid
         container
-        xs={12}
         sx={{
           display: "flex",
           marginTop: "3em",
           px: { xs: 3, md: 8 },
         }}
       >
-        {mockData.map(({ id, name, description, thumbnail }: character) => {
+        {results.map(({ id, name, description, thumbnail }: character) => {
           return (
             <Grid item key={id} xs={12} sm={6} md={4} lg={3}>
               <CharacterCard
-                image={`${thumbnail.path}/standard_fantastic.${thumbnail.extension}`}
-                description={description}
+                image={
+                  `${thumbnail.path}/standard_fantastic.${thumbnail.extension}` ||
+                  ""
+                }
                 name={name}
               />
             </Grid>
           );
         })}
       </Grid>
+      <Grid
+        container
+        sx={{ width: "100%", display: "flex", justifyContent: "center" }}
+      >
+        <Grid item component="div" ref={isAtBottom} sx={{ m: 2 }}>
+          {onScreen && (
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "primary.med", color: "#fff" }}
+            >
+              Loading more content...
+            </Button>
+          )}
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
 export default Characters;
+
+export const getStaticProps = async () => {
+  try {
+    const data = await getMarvelData("/characters", 25);
+
+    return {
+      props: {
+        data: data.data.results,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: { error },
+      },
+    };
+  }
+};
