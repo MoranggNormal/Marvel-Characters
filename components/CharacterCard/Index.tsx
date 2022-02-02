963e6908a88156dc17532202407667a20e3d2109
Euller Peixoto <peixotoeuller500@gmail.com>
2022-02-02 04:48:37 -0300

    update characters cards UI

--- components/CharacterCard/Index.tsx
+++ components/CharacterCard/Index.tsx
@@ -12,29 +12,28 @@ type character = {
 
 export default function CharacterCard({ image, name }: character) {
   return (
-    <Card
-      sx={{
-        m: 1,
-        borderRadius: "0.2em",
-      }}
-      elevation={2}
-    >
-      <CardHeader
-        sx={{
-          backgroundColor: "primary.med",
-          color: "primary.main",
-          cursor: "default",
-        }}
-        title={name}
-      />
-      <CardMedia component="img" height="194" image={image} alt={name} />
-      <CardContent>
-        <Link href="/" passHref>
-          <Typography component="a" variant="body2" color="text.link">
-            More info about {name} →
-          </Typography>
-        </Link>
-      </CardContent>
-    </Card>
+    <Link href="/" passHref>
+      <a>
+        <Card
+          sx={{
+            m: 1,
+            borderRadius: "0.2em",
+            border: "2px solid #E53935",
+            cursor: "pointer",
+          }}
+          elevation={2}
+        >
+          <CardHeader
+            sx={{
+              backgroundColor: "primary.med",
+              color: "primary.main",
+            }}
+            titleTypographyProps={{ variant: "caption" }}
+            title={name}
+          />
+          <CardMedia component="img" height="194" image={image} alt={name} />
+        </Card>
+      </a>
+    </Link>
   );
 }
